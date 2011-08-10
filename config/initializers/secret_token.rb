864a20a380d09663ce2d40615d0fe3a48d240d2f
root <root@studiouslyLynx.(none)>
2011-08-10 05:13:39 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project2::Application.config.secret_token = '3e30f02cc11ee0261312a7c004167899c870bc03045034ed4960f41fcac0045cd3c1ea85c57fcd3fbe243de9b9b02725f88196466c8f8728eac0a42550e749d5'
